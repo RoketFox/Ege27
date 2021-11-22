bc8055f3c0dd913cd592af5b7b40215038f9c480
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 02:21:39 +0300

    reads and prints nums

--- ege/main.cpp
+++ ege/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void number_of_products_div()
-{///
-/**
-1. считать и запомнить входные числа
-2. вывести все пары чисел
-3. вместо вывода пар посчитать произведение
-4. вывести пары, произведения которых равны 22
-5. только кол-во пар
-*/
+void number_products_div_ineff_2()
+{
+    /**
+    1. считать и запомнить входные числа
+    2. вывести все пары чисел
+    3. вместо вывода пар посчитать произведение
+    4. вывести пары, произведения которых равны 22
+    5. только кол-во пар
+    */
+
+        int n(0);
+    cin >> n;
+
+    vector<int> numbers(n,0);
+
+    for (int i(0);i<n;i++)
+        cin >> numbers[i];
+    for (int i(0);i<n;i++)
+        cout<< numbers[i]<<" ";
 }
 
 int main()
 {
+    number_products_div_ineff_2();
     return 0;
 }
