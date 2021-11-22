6588989f87d98fdbd9da7d1f9588157e774eea72
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 01:36:29 +0300

    prints multiplications

--- ege/main.cpp
+++ ege/main.cpp
@@ -22,7 +22,7 @@ void number_sums_div_ineff_2()
 
     for (int i(0);i<n-1;i++)
         for (int j(i+1);j<n;j++)
-            cout << numbers[i] << " " << numbers[j] << endl;
+            cout << numbers[i] * numbers[j] << endl;
 }
 
 int main()
