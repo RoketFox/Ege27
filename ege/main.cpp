7580d2066e4a720c522e6885646eeb8b6b902a7e
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 02:24:27 +0300

    print pairs

--- ege/main.cpp
+++ ege/main.cpp
@@ -20,8 +20,10 @@ void number_products_div_ineff_2()
 
     for (int i(0);i<n;i++)
         cin >> numbers[i];
-    for (int i(0);i<n;i++)
-        cout<< numbers[i]<<" ";
+
+    for (int i(0);i<n-1;i++)
+        for (int j(i+1);j<n;j++)
+            cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
