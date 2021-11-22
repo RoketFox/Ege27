d2153b5da812dc552b1c7f95b66ddf09d7f7bade
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 01:44:49 +0300

    prints max sum with multiplications

--- ege/main.cpp
+++ ege/main.cpp
@@ -23,8 +23,8 @@ void number_sums_div_ineff_2()
 
     for (int i(0);i<n-1;i++)
         for (int j(i+1);j<n;j++)
-            if ((numbers[i] + numbers[j]) % 7 == 0)
-                cout << numbers[i] + numbers[j] << endl;
+            if (((numbers[i] + numbers[j]) % 7 == 0) && ((numbers[i] * numbers[j]) % 3 == 0))
+                cout << numbers[i] + numbers[j] << " " << numbers[i] * numbers[j] << endl;
 }
 
 int main()
