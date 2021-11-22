1a9ce8ea50e866af0e6d6ec42b1ec7eaf901136b
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 02:01:58 +0300

    prints max sum

--- ege/main.cpp
+++ ege/main.cpp
@@ -14,18 +14,35 @@ void number_sums_div_ineff_2()
     5. вывести только наибольшую сумму подходящую по условию
     */
 
-        int n(0);
+    int n(0);
+    int currsum(0);
+    int finsum(0);
+
     cin >> n;
     vector<int> numbers(n);
+
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    cout << endl;
 
     for (int i(0);i<n-1;i++)
+    {
         for (int j(i+1);j<n;j++)
+        {
             if (((numbers[i] + numbers[j]) % 7 == 0) && ((numbers[i] * numbers[j]) % 3 == 0))
-                cout << numbers[i] + numbers[j] << " " << numbers[i] * numbers[j] << endl;
+            {
+                currsum = numbers[i] + numbers[j];
+                if (currsum > finsum) finsum = currsum;
+
+            }
+
+        }
+    }
+    if (finsum == 0) cout << "FAIL" << endl;
+    else cout << finsum << endl;
+
 }
+
+
 
 int main()
 {
