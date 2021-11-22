591f3d95fc8586c84686f129f903ed01911239fb
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 01:29:33 +0300

    reads and prints nums

--- ege/main.cpp
+++ ege/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,9 +14,17 @@ void number_sums_div_ineff_2()
     5. вывести только наибольшую сумму подходящую по условию
     */
 
+        int n(0);
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
 {
+    number_sums_div_ineff_2();
     return 0;
 }
