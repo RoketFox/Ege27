8aa828918795a403dd7a2e5f8e327bb7df85e249
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-23 01:32:16 +0300

    print pairs

--- ege/main.cpp
+++ ege/main.cpp
@@ -19,8 +19,10 @@ void number_sums_div_ineff_2()
     vector<int> numbers(n);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    for (int i(0); i < n; i++)
-        cout << numbers[i] << " ";
+
+    for (int i(0);i<n-1;i++)
+        for (int j(i+1);j<n;j++)
+            cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
