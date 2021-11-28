9efc0c0303c9f6c1bdd73553e68f04c5b89ad7a3
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-28 13:06:21 +0300

    prints max odd and max even

--- ege/main.cpp
+++ ege/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void number_sums_div_eff_1()
+{
+       int n; cin >> n;
+       int odd(0);
+       int even(0);
+
+       for (int i = 0; i < n; i++)
+       {
+           int a(0); cin >> a;
+           if (a % 2 == 1 && a > odd)  odd  = a;
+           if (a % 2 == 0 && a > even) even = a;
+       }
+       if (odd > 0 && even > 0) cout << odd + even << endl;
+       else cout << -1 << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_sums_div_eff_1();
     return 0;
 }
