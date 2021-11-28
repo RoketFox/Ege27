0b7323a31bbae11ed01e90dcdcdb6d5978ba622a
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-28 14:13:20 +0300

    in work

--- ege/main.cpp
+++ ege/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+void number_sums_div_eff_2()
+{
+       int n; cin >> n;
+       int divisible(0);
+       int summand  (0);
+
+       for (int i = 0; i < n; i++)
+       {
+           int a(0); cin >> a;
+           if (a % 3 == 0 && a > divisible && (a + summand) % 7 == 0) divisible = a;
+           else if ((a + divisible) % 7 == 0 && a > summand) summand = a;
+       }
+       cout << divisible << " " << summand << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_sums_div_eff_2();
     return 0;
 }
