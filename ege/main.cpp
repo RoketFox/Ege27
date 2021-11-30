e2a3cedc1208cb98fd79c73290f277b5864936a1
Max_the_Fox <maksim.dmd05@gmail.com>
2021-11-30 12:31:31 +0300

    added plan

--- ege/main.cpp
+++ ege/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void min_sum_min_distance_ineff_1()
+{
+    /**
+    1. считать все числа
+    2. вывести все числа
+    3. вывести все пары
+    4. вывести только достаточно далёкие пары
+    5. вместо вывода пары - сумма
+    б. найти минимальную сумму
+    */
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_min_distance_ineff_1();
     return 0;
 }
