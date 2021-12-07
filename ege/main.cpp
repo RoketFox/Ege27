c484b94f6255b9e230719ce34a6e2907de6572ca
Max_the_Fox <maksim.dmd05@gmail.com>
2021-12-07 10:50:02 +0300

    added development plan

--- ege/main.cpp
+++ ege/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void all_pairs_with_diff_index_ineff()
+{
+    /**
+    1.считать все числа
+    2.вывести пары
+    3.вывести пары удовлетворяющие первому условию
+    4.вывести пары удовлетворяющие второму условию
+    5.вывести пары удовлетворяющие обеим условиям
+    6.вывести ответ
+    */
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    all_pairs_with_diff_index_ineff();
     return 0;
 }
