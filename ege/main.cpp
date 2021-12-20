56a6c56ebbffaa87e1d5360f4c85c6f6f990c829
Max_the_Fox <maksim.dmd05@gmail.com>
2021-12-21 00:48:24 +0300

    all works good

--- ege/main.cpp
+++ ege/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <array>
 
-using namespace std;
+void num_sum_max_distance_eff_2()
+{
+    int minLen = 7;
+    int mult = 28;
+
+    int N = 0;
+    std::cin >> N;
+    
+    int fin = 0;
+
+    std::array <int,7> mainBuf;
+    std::array <int,28> helpBuf;
+    helpBuf.fill(0);
+
+    for (int i = 0; i < minLen; i++)
+    {
+        std::cin >> mainBuf[i];
+    }
+    
+    for (int i = minLen; i < N; i++)
+    {
+        helpBuf[mainBuf[0] % mult] += 1;
+
+        for (int j = 0; j < minLen - 1; j++)
+        {
+            mainBuf[j] = mainBuf[j + 1];
+        }
+
+        int a = 0;
+        std::cin >> a;
+
+        mainBuf[minLen - 1] = a;
+        fin += helpBuf[(mult - (a % mult)) % mult];
+    }
+    std::cout << fin;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_sum_max_distance_eff_2();
     return 0;
 }
