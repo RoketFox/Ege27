a4f8292e7e4cc13b015d117e0fd8ee3da8c3c0fa
Max_the_Fox <maksim.dmd05@gmail.com>
2021-12-21 12:58:00 +0300

    prints good trio all works fine

--- ege/main.cpp
+++ ege/main.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 
 
-void max_3nums_div_by4()
+void max_3nums_div_by4_ineff()
 {
     int mult = 4;
 
     int N = 0;
     std::cin >> N;
+
+    int fin1 = 0, fin2 = 0, fin3 = 0;
 
     std::vector<int> numbers(N,0);
 
@@ -20,11 +22,17 @@ void max_3nums_div_by4()
         for (int j = i+1; j < N; j++)
             for (int k = j+1; k < N; k++)
                 if ((numbers[i] * numbers[j] * numbers[k]) % mult == 0)
-                    std::cout << numbers[i] <<" "<< numbers[j] <<" "<< numbers[k] <<std::endl;
+                {
+                    fin1 = std::max(fin1, numbers[i]);
+                    fin2 = std::max(fin2, numbers[j]);
+                    fin3 = std::max(fin3, numbers[k]);
+                }
+                    
+    std::cout << fin1 <<" "<< fin2 <<" "<< fin3 <<std::endl;
 }
 
 int main()
 {
-    max_3nums_div_by4();
+    max_3nums_div_by4_ineff();
     return 0;
 }
