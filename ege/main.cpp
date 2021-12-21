df28854a3948be4c2f6ec188ee386b9f8a9fdc63
Max_the_Fox <maksim.dmd05@gmail.com>
2021-12-21 12:37:10 +0300

    reads and prints numbers

--- ege/main.cpp
+++ ege/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
 
 void max_3nums_div_by4()
 {
-    
+    int N = 0;
+    std::cin >> N;
+
+    std::vector<int> numbers(N,0);
+
+    for (int i = 0;i<N;i++)
+    {
+        std::cin >> numbers[i];
+    }
+
+    for (int i = 0;i < N;i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
 }
 
 int main()
